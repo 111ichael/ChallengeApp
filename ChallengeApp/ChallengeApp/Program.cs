981ebc0b93c76fc1b7f10bbe0e132193e2a8c3d1
111ichał <111otorsport@gmail.com>
2023-03-28 20:20:11 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = ("Ewa");
+char sex = 'K';
+int age = 33;
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 33 && name == ("Ewa"))
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == 'M' && age <18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
